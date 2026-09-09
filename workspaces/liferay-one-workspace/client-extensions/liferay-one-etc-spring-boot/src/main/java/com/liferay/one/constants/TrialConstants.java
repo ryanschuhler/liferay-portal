832b3b223afb981dc72c145f8394abb7cec2ea1a
Ryan Schuhler <ryan.schuhler@liferay.com>
2026-09-09 16:43:55 -0700

    LPD-102704 Start CMP and DSR self service trials for 90 days
    The trial length is the only thing that separates a self service trial from the sales issued order it mirrors, so it is resolved from the order type in one place rather than being stamped by the browser.

---
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/constants/TrialConstants.java
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.one.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Ryan Schuhler
+ */
+public class TrialConstants {
+
+	public static final int DURATION_DAYS_DEFAULT = 7;
+
+	public static final int DURATION_DAYS_SELF_SERVICE = 90;
+
+	public static final String ORDER_TYPE_EXTERNAL_REFERENCE_CODE_CMP_TRIAL =
+		"CMP_TRIAL";
+
+	public static final String ORDER_TYPE_EXTERNAL_REFERENCE_CODE_DSR_TRIAL =
+		"DSR_TRIAL";
+
+	public static int getDurationDays(String orderTypeExternalReferenceCode) {
+		if (isSelfService(orderTypeExternalReferenceCode)) {
+			return DURATION_DAYS_SELF_SERVICE;
+		}
+
+		return DURATION_DAYS_DEFAULT;
+	}
+
+	public static boolean isSelfService(
+		String orderTypeExternalReferenceCode) {
+
+		return _ORDER_TYPE_EXTERNAL_REFERENCE_CODES_SELF_SERVICE.contains(
+			orderTypeExternalReferenceCode);
+	}
+
+	private static final List<String>
+		_ORDER_TYPE_EXTERNAL_REFERENCE_CODES_SELF_SERVICE = Arrays.asList(
+			ORDER_TYPE_EXTERNAL_REFERENCE_CODE_CMP_TRIAL,
+			ORDER_TYPE_EXTERNAL_REFERENCE_CODE_DSR_TRIAL);
+
+}
